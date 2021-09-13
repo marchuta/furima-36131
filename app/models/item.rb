32a81d89241897ac1964a605383869d179e04b2d
marchuta <86639141+marchuta@users.noreply.github.com>
2021-09-13 18:36:52 +0900

    active hashを用いたデータ保存

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,24 @@
 class Item < ApplicationRecord
 
   validates :name, presence: true
-  validates :description, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :carriage_id, presence: true
-  validates :region_id, presence: true
-  validates :day_id, presence: true
+
+
   validates :price, presence: true
   validates :image, presence: true
 
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :carriage_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :region_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :day_id, numericality: { other_than: 1 , message: "can't be blank"}
+
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :carriage
+  belongs_to :region
+  belongs_to :day
 end
