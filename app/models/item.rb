bb836a4185add6953bebcccb0cf954424e7a335e
marchuta <86639141+marchuta@users.noreply.github.com>
2021-09-17 14:41:27 +0900

    レビュー対応

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   validates :name, presence: true
 
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, presence: true
+  validates :price, numericality: { only_integer: true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, presence: true
   validates :image, presence: true
   validates :description, presence: true
 
