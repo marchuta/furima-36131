c382efac48b7efaef449da9296e73d0e57bbcca1
marchuta <86639141+marchuta@users.noreply.github.com>
2021-09-29 16:47:25 +0900

    edit item information

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new] # :edit, #:destroy ]
+  before_action :authenticate_user!, only: [:new ,:edit]#, :destroy ]
+  before_action :move_to_index, only: [:edit, :update]#, :destroy ]
 
   def index
     @items = Item.all.order(:created_at)
@@ -19,6 +20,23 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit 
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+
+    if @item.save
+      redirect_to root_path
+    else
+      render :edit
+    end
+
+    
+  end
+
   def show
     @item = Item.find(params[:id])
   end
@@ -29,4 +47,12 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :description, :category_id, :condition_id, :carriage_id, :region_id, :day_id, :price,
                                  :image).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless current_user == @item.user
+      redirect_to action: :index 
+    end
+  end
+
 end
