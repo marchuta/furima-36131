b3feabb972f53bb52dec7b1f9ab688cd1242c45a
marchuta <86639141+marchuta@users.noreply.github.com>
2021-10-06 12:30:11 +0900

    addied functions

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,22 +1,18 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, except: :index
+  before_action :move_to_index, only: [:index]
+
   def index
-
-   @item = Item.find(params[:item_id])
-
-   @order_address = OrderAddress.new
-   
-
-  end
-
-
-  def create
-    
     @item = Item.find(params[:item_id])
 
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
 
     @order_address = OrderAddress.new(order_params)
-    
+
     if @order_address.valid?
 
       pay_item
@@ -26,22 +22,27 @@ class OrdersController < ApplicationController
     else
       render :index
     end
-
   end
-    
 
   private
 
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  
-      Payjp::Charge.create(
-        amount: @item.price,  
-        card: order_params[:token],    
-        currency: 'jpy'                 
-      )
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 
   def order_params
-    params.require(:order_address).permit(:post_code, :region_id, :city, :block_number, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id,  token: params[:token])
+    params.require(:order_address).permit(:post_code, :region_id, :city, :block_number, :building, :phone_number).merge(
+      user_id: current_user.id, item_id: @item.id, token: params[:token]
+    )
+  end
+
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user == @item.user || @item.order.present?
   end
 end
